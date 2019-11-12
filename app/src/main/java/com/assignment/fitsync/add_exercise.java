8d7906cb3a311885ed6e78271b6708d2d0dd4ebb
fpathan <fpathan@ucsc.edu>
2019-11-11 20:21:14 -0800

    have adding to collection working need to do getting data from textfield and update what data is already present in the document(user)

--- app/src/main/java/com/assignment/fitsync/add_exercise.java
+++ app/src/main/java/com/assignment/fitsync/add_exercise.java
@@ -1,19 +1,69 @@
 package com.assignment.fitsync;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.CollectionReference;
+
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreSettings;
+
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+
 public class add_exercise extends AppCompatActivity {
+
+    private static final String TAG = "add_exercise";
+
+    public FirebaseFirestore db = FirebaseFirestore.getInstance();
+    public CollectionReference members = db.collection("members");
+
+
+    public void setup() {
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        FirebaseFirestoreSettings settings = new FirebaseFirestoreSettings.Builder()
+                .setPersistenceEnabled(true)
+                .build();
+        db.setFirestoreSettings(settings);
+    }
+    public void setupCacheSize() {
+        // [START fs_setup_cache]
+        FirebaseFirestoreSettings settings = new FirebaseFirestoreSettings.Builder()
+                .setCacheSizeBytes(FirebaseFirestoreSettings.CACHE_SIZE_UNLIMITED)
+                .build();
+        db.setFirestoreSettings(settings);
+        // [END fs_setup_cache]
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_exercise);
+
+
+
+
+
+
+
+
+
+
         Spinner spinner = (Spinner) findViewById(R.id.day_spinner);
 // Create an ArrayAdapter using the string array and a default spinner layout
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
@@ -23,6 +73,36 @@ public class add_exercise extends AppCompatActivity {
 // Apply the adapter to the spinner
         spinner.setAdapter(adapter);
     }
+
+    public void add(View view) {
+
+        Map<String, Object> data1 = new HashMap<>();
+        List exercises = new ArrayList();
+        data1.put("exercise", "deadlift");
+        data1.put("sets", 3);
+        data1.put("reps", 10);
+        members.document("BroScienceLife").set(data1)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Log.d(TAG, "DocumentSnapshot successfully written!");
+                        System.out.println("|||||||||||||||||||||||||||||||||||||||||||||||||||||");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        System.out.println("*******************************************************");
+                        Log.w(TAG, "Error writing document", e);
+                    }
+                });
+
+        //intent to start other activity
+        Intent go_back = new Intent(this, workout_screen.class);
+        startActivity(go_back);
+
+    }
+
 
     public void goBack(View view) {
 
